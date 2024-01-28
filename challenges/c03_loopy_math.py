5352c542383a03b57ed5548cf114436bc981aefd
Nicholas Kruckenberg <nkruckenberg@gmail.com>
2024-01-28 14:44:30 -0600

    init loopy math challenge

---
+++ challenges/c03_loopy_math.py
@@ -0,0 +1,24 @@
+def for_loopy_adding(a, b):
+    """Add two numbers together using a for loop."""
+    pass
+
+
+def while_loopy_adding(a, b):
+    """Add two numbers together using a while loop."""
+    pass
+
+
+def loopy_multiplying(a, b):
+    """
+    Multiply two numbers together using a loop.
+    Be sure your function can handle negative numbers.
+    """
+    pass
+
+
+def loopy_remainder_division(divisor, dividend):
+    """
+    Divide two numbers using a loop. Do NOT use the regular division operator.
+    Return a tuple containing the whole-number quotient and the remainder.
+    """
+    pass
