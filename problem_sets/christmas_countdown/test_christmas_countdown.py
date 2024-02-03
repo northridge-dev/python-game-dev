1c9171e59aef5842cd5b4aefbe6c22b16d068ff3
Nicholas Kruckenberg <nkruckenberg@gmail.com>
2024-02-03 11:44:31 -0600

    del leap year sensitive test

--- problem_sets/christmas_countdown/test_christmas_countdown.py
+++ problem_sets/christmas_countdown/test_christmas_countdown.py
@@ -1,5 +1,5 @@
 import pytest
-from christmas_countdown import get_days_until_christmas
+from christmas_countdown_impl import get_days_until_christmas
 
 """
 These are the tests that will be run to check your work. You can
@@ -10,9 +10,6 @@ add a test if you'd like, but don't change the existing tests.
 def test_christmas_countdown():
     assert get_days_until_christmas(12, 25, 2024) == 0
     assert get_days_until_christmas(12, 24, 2024) == 1
-
-    # handle a day after Christmas
-    assert get_days_until_christmas(12, 27, 2024) == 363
 
     # handle a day after leap day
     assert get_days_until_christmas(11, 11, 2024) == 44
@@ -29,7 +26,7 @@ def test_christmas_countdown_unhandled_leap_year():
 
 
 # Comment out the line below after completing the first bonus challenge
-@pytest.mark.skip(reason="include test only AFTER finishing bonus challenge #1")
+# @pytest.mark.skip(reason="include test only AFTER finishing bonus challenge #1")
 def test_christmas_countdown_leap_year():
     # handle a day after Christmas when following year is a leap year
     assert get_days_until_christmas(12, 27, 2023, True) == 364
